0abc11f8f466daaecd14519d2106f119df2199ff
Bas Velthuizen <bas.velthuizen@marviq.com>
2017-01-19 14:38:58 +0100

    Now generating uuid in response

--- handlers.go
+++ handlers.go
@@ -3,13 +3,16 @@ package main
 import (
 	"net/http"
 
+	"github.com/pborman/uuid"
+
 	"github.com/unrolled/render"
 )
 
 func createMatchHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		formatter.JSON(w,
-			http.StatusCreated,
-			struct{ Test string }{"This is a test"})
+		guid := uuid.New()
+		w.Header().Add("Location", "/matches/"+guid)
+
+		formatter.JSON(w, http.StatusCreated, &newMatchResponse{ID: guid})
 	}
 }
--- handlers_test.go
+++ handlers_test.go
@@ -1,52 +1,79 @@
 package main
 
 import (
-    "bytes"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "net/http/httptest"
-    "testing"
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-    "github.com/unrolled/render"
+	"strings"
+
+	"encoding/json"
+
+	"github.com/unrolled/render"
+)
+
+const (
+	fakeMatchLocationResult = "/matches/5a003b78-409e-4452-b456-a6f0dcee05bd"
 )
 
 var (
-    formatter = render.New(render.Options{
-        IndentJSON: true,
-    })
+	formatter = render.New(render.Options{
+		IndentJSON: true,
+	})
 )
 
 func TestCreateMatch(t *testing.T) {
-    client := &http.Client{}
-    server := httptest.NewServer(
-    http.HandlerFunc(createMatchHandler(formatter)))
-    defer server.Close()
+	client := &http.Client{}
+	server := httptest.NewServer(
+		http.HandlerFunc(createMatchHandler(formatter)))
+	defer server.Close()
 
-    body := []byte("{\n   \"gridsize\": 19, \n  \"players\": [\n    {\n\"color\": \"white\",\n      \"name\": \"bob\"\n    },\n    {\n      \"color\": \"black\",\n      \"name\": \"alfred\"\n    }\n  ]\n}")
-    
-    req, err := http.NewRequest("POST",
-        server.URL, bytes.NewBuffer(body))
-    if err != nil {
-        t.Errorf("Error in creating POST request for createMatchHandler %v", err)        
-    }
-    req.Header.Add("Content-Type", "application/json")
+	body := []byte("{\n   \"gridsize\": 19, \n  \"players\": [\n    {\n\"color\": \"white\",\n      \"name\": \"bob\"\n    },\n    {\n      \"color\": \"black\",\n      \"name\": \"alfred\"\n    }\n  ]\n}")
 
-    res, err :=client.Do(req)
-    if err != nil {
-        t.Errorf("Error in POST to createMatchHandler: %v", err)
-    }
+	req, err := http.NewRequest("POST",
+		server.URL, bytes.NewBuffer(body))
+	if err != nil {
+		t.Errorf("Error in creating POST request for createMatchHandler %v", err)
+	}
+	req.Header.Add("Content-Type", "application/json")
 
-    defer res.Body.Close()
+	res, err := client.Do(req)
+	if err != nil {
+		t.Errorf("Error in POST to createMatchHandler: %v", err)
+	}
 
-    payload, err := ioutil.ReadAll(res.Body)
-    if err != nil {
-        t.Errorf("Error reading response body: %v", err)
-    }
+	defer res.Body.Close()
 
-    if res.StatusCode != http.StatusCreated {
-        t.Errorf("Expected response status 201, received %s", res.Status)
-    }
-    
-    fmt.Printf("Payload: %s", string(payload))
+	payload, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("Error reading response body: %v", err)
+	}
+
+	if res.StatusCode != http.StatusCreated {
+		t.Errorf("Expected response status 201, received %s", res.Status)
+	}
+
+	loc, headerOk := res.Header["Location"]
+	if !headerOk {
+		t.Error("Location header is not set")
+	} else {
+		if !strings.Contains(loc[0], "/matches/") {
+			t.Errorf("Location header should comtain '/matches/'")
+		}
+		if len(loc[0]) != len(fakeMatchLocationResult) {
+			t.Errorf("Location value does not contain guid of new match")
+		}
+
+	}
+
+	var matchResponse newMatchResponse
+	err = json.Unmarshal(payload, &matchResponse)
+	if err != nil {
+		t.Errorf("Could not unmarshal payload into newMatchResponse object")
+	}
+	if matchResponse.ID == "" || !strings.Contains(loc[0], matchResponse.ID) {
+		t.Errorf("matchResponse.ID '%s' does not match Location header '%s'", matchResponse.ID, loc[0])
+	}
 }
